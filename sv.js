a7edc240ae4521891efa56e1350ac620c7e52fc4
Kirill <Evstkire@mail.ru>
2025-10-31 13:05:28 +0700

    Исправлены ошибки

--- sv.js
+++ sv.js
@@ -1,17 +1,39 @@
-const CACHE = "budget-v1";
-const FILES = [
-  "index.html",
-  "style.css",
-  "script.js",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png",
+const CACHE = "budget-v6.1";
+const FILES_TO_CACHE = [
+  "/Expense-Planner/",
+  "/Expense-Planner/index.html",
+  "/Expense-Planner/style.css",
+  "/Expense-Planner/script.js",
+  "/Expense-Planner/manifest.json",
+  "/Expense-Planner/icon-192.png",
+  "/Expense-Planner/icon-512.png",
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) return res;
+      return fetch(e.request).catch(() => {
+        return caches.match("/Expense-Planner/index.html");
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE) return caches.delete(cache);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
